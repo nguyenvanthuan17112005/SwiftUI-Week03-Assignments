316e15c000068f3930d52b5c280c4dab4bbc9b68
nguyenvanthuan17112005 <thuannv2563@ut.edu.vn>
2025-12-20 11:37:04 +0700

    Init Commit

--- 01-Nullable/NullableDemo/NullableDemo/ContentView.swift
+++ 01-Nullable/NullableDemo/NullableDemo/ContentView.swift
@@ -1,24 +1,29 @@
-//
-//  ContentView.swift
-//  NullableDemo
-//
-//  Created by Nguyễn Văn Thuận on 20/12/25.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email: String? = nil
+    @State private var inputText: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 20){
+            Text("Nullable / Optional Demo")
+                .font(.title)
+                .bold()
+            TextField("Nhập email", text: $inputText)
+                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal)
+            Button("Lưu email"){
+                email = inputText.isEmpty ? nil : inputText
+            }
+            Divider()
+            Text("Email hiện tại:")
+                .font(.headline)
+            Text(email ?? "Chưa có email")
+                .foregroundColor(email == nil ? .gray : .blue)
         }
         .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
