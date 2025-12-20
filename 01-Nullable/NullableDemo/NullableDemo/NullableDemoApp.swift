fb04fe098da9aae2e4f8999a9a8fbfd7fec2c1da
nguyenvanthuan17112005 <thuannv2563@ut.edu.vn>
2025-12-20 11:36:17 +0700

    Init Commit

---
+++ 01-Nullable/NullableDemo/NullableDemo/NullableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NullableDemoApp.swift
+//  NullableDemo
+//
+//  Created by Nguyễn Văn Thuận on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NullableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
